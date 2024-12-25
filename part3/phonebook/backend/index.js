46ea828b08ba11d39fa350f1ef91ef5acc4aca72
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2024-12-25 11:03:26 +0530

    fix(part3/phonebook): fix deployment port issue. (#10)
    - Use correct port variable for deployment.

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -92,7 +92,7 @@ app.post("/api/persons", (req, res) => {
   res.status(201).json(person);
 });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
